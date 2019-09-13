36a8d47f80a07662d54fd685db1f0887fe2482a6
Allan96 <allan.souza.silva@usp.br>
2019-09-12 22:54:13 -0300

    Importando o React.component

--- react-ninja/webpack/src/app.js
+++ react-ninja/webpack/src/app.js
@@ -1,8 +1,8 @@
 'use strict'
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
 
-class App  extends React.Component{
+class App  extends Component{
     render(){
         return (
             <div>
